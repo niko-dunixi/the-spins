53e3ce5efd6de820d5f74f74a1fc85d719680656
Paul Baker <paul.nelson.baker@gmail.com>
2019-08-12 12:22:43 -0600

    Using matricies instead of just raw x/y variables

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/paul-nelson-baker/spins
 
 go 1.12
 
-require github.com/go-vgo/robotgo v0.0.0-20190807164536-50af1aa75f3b
+require (
+	github.com/go-vgo/robotgo v0.0.0-20190807164536-50af1aa75f3b
+	gonum.org/v1/gonum v0.0.0-20190808205415-ced62fe5104b
+)
--- main.go
+++ main.go
@@ -1,12 +1,35 @@
 package main
 
 import (
+	"math"
+
 	"github.com/go-vgo/robotgo"
+	"gonum.org/v1/gonum/mat"
 )
 
 func main() {
-	width, height := robotgo.GetScreenSize()
+	midScreenMatrix := createMidScreenMatrix()
 	originalX, originalY := robotgo.GetMousePos()
-	robotgo.MoveMouseSmooth(width/2, height/2)
+	robotgo.MoveMouseSmooth(int(midScreenMatrix.At(0, 0)), int(midScreenMatrix.At(0, 1)))
 	robotgo.MoveMouse(originalX, originalY)
+}
+
+func createMidScreenMatrix() mat.Matrix {
+	screenSizeMatrix := createScreenSizeMatrix()
+	midScreenMatrix := mat.NewDense(1, 2, nil)
+	midScreenMatrix.Scale(0.5, screenSizeMatrix)
+	return midScreenMatrix
+}
+
+func createScreenSizeMatrix() mat.Matrix {
+	width, height := robotgo.GetScreenSize()
+	values := []float64{float64(width), float64(height)}
+	return mat.NewDense(1, 2, values)
+}
+
+func createRotationMatrix(theta float64) mat.Matrix {
+	// Row major order of the rotation matrix
+	// https://en.wikipedia.org/wiki/Rotation_matrix
+	values := []float64{math.Cos(theta), math.Sin(theta), -math.Sin(theta), math.Cos(theta)}
+	return mat.NewDense(2, 2, values)
 }
