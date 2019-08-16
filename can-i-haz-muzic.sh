f4fa818bc3ab34aee4710adf768a8f1ce8e87cf6
Paul Baker <paul.nelson.baker@gmail.com>
2019-08-16 09:58:02 -0600

    Script to download the audio needed for shinanigans

--- can-i-haz-muzic.sh
+++ can-i-haz-muzic.sh
@@ -6,8 +6,9 @@ song_file="nsp-you-spin-me-cover.mp3"
 loop_file="spin-loop.mp3"
 
 if [ ! -f "./${song_file}" ]; then
-  youtube-dl "${song_url}" -x --audio-format mp3 -o "${song_file}"
+  youtube-dl "${song_url}" --extract-audio --audio-format mp3 --exec "mv {} ${song_file}"
 fi
 if [ ! -f "./${loop_file}" ]; then
-  ffmpeg -i "${song_file}" -ss 00:01:16.00 -to 00:01:23.00 -c copy "./${loop_file}"
+  ffmpeg -i "${song_file}" -ss 00:01:15.50 -to 00:01:23.00 -c copy "./${loop_file}"
 fi
+echo "I haz muzic: ${loop_file}"
