923c79925a566197fb0797a779922dd84368be32
Paul Freakn Baker <paul.nelson.baker@gmail.com>
2020-05-28 20:28:50 -0600

    Auto accepting

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -9,10 +9,10 @@ set -ex
 # sudo apt-get install nx-x11proto-xext-dev
 
 # robotgo
-sudo apt-get install gcc libc6-dev
-sudo apt-get install libx11-dev xorg-dev libxtst-dev libpng++-dev
-sudo apt-get install xcb libxcb-xkb-dev x11-xkb-utils libx11-xcb-dev libxkbcommon-x11-dev
-sudo apt-get install libxkbcommon-dev
-sudo apt-get install xsel xclip
+sudo apt-get install -y gcc libc6-dev
+sudo apt-get install -y libx11-dev xorg-dev libxtst-dev libpng++-dev
+sudo apt-get install -y xcb libxcb-xkb-dev x11-xkb-utils libx11-xcb-dev libxkbcommon-x11-dev
+sudo apt-get install -y libxkbcommon-dev
+sudo apt-get install -y xsel xclip
 # beep
-sudo apt-get install libasound2-dev
+sudo apt-get install -y libasound2-dev
